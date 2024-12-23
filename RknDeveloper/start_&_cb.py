2bcb29e2883e7f0d5b2f8a209eea92b79911b141
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-23 16:44:54 +0530

    Update start_&_cb.py

--- RknDeveloper/start_&_cb.py
+++ RknDeveloper/start_&_cb.py
@@ -45,14 +45,13 @@ async def start_commond(bot, m :Message):
             ))
             
     await rkn_botz.add_user(bot, m)   
-    await m.reply_photo(photo=rkn1.RKN_PIC, caption="**Hᴇy, {}!\n\nI'ᴍ Aɴ Aᴜᴛᴏ Aᴘᴘʀᴏᴠᴇ [Aᴅᴍɪɴ Jᴏɪɴ Rᴇǫᴜᴇsᴛs]({}) Bᴏᴛ.\nI Cᴀɴ Aᴘᴘʀᴏᴠᴇ Usᴇʀs Iɴ Cʜᴀɴɴᴇʟs & Gʀᴏᴜᴘs.Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Aɴᴅ Gʀᴏᴜᴘ ᴀɴᴅ Pʀᴏᴍᴏᴛᴇ Mᴇ Tᴏ Aᴅᴍɪɴ Wɪᴛʜ Aᴅᴅ Mᴇᴍʙᴇʀs Pᴇʀᴍɪssɪᴏɴ.\n\n__Pᴏᴡᴇʀᴅ Bʏ : @RknDeveloper__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=InlineKeyboardMarkup([[
+    await m.reply_video(video=rkn1.RKN_PIC, caption="**Hᴇy, {}!\n\nI'ᴍ Aɴ Aᴜᴛᴏ Aᴘᴘʀᴏᴠᴇ [Aᴅᴍɪɴ Jᴏɪɴ Rᴇǫᴜᴇsᴛs]({}) Bᴏᴛ.\nI Cᴀɴ Aᴘᴘʀᴏᴠᴇ Usᴇʀs Iɴ Cʜᴀɴɴᴇʟs & Gʀᴏᴜᴘs.Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Aɴᴅ Gʀᴏᴜᴘ ᴀɴᴅ Pʀᴏᴍᴏᴛᴇ Mᴇ Tᴏ Aᴅᴍɪɴ Wɪᴛʜ Aᴅᴅ Mᴇᴍʙᴇʀs Pᴇʀᴍɪssɪᴏɴ.\n\n__Pᴏᴡᴇʀᴅ Bʏ : @CinemaKalavaraTG__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=InlineKeyboardMarkup([[
                 #⚠️ don't change source code & source link ⚠️ #
                 InlineKeyboardButton("✛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀɴɴᴇʟ ࿇", url=f"https://t.me/{bot.username}?startchannel=Bots4Sale&admin=invite_users+manage_chat")
                     ],[
                 InlineKeyboardButton("✛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ࿇", url=f"https://t.me/{bot.username}?startgroup=Bots4Sale&admin=invite_users+manage_chat")
                 ],[
-                InlineKeyboardButton("💥 Uᴘᴅᴀᴛᴇ 💥", url="https://t.me/+sT56Fh1VXZ81ZWRl")
-                ],[
+                InlineKeyboardButton("💥 Uᴘᴅᴀᴛᴇ 💥", url="https://t.me/+sT56Fh1VXZ81ZWRl"),
                 InlineKeyboardButton("🔎 Sᴜᴘᴘᴏʀᴛ 🔍", url="https://t.me/+sT56Fh1VXZ81ZWRl")
             ]]))
             
@@ -65,8 +64,7 @@ async def start_query(bot, cb : CallbackQuery):
                     ],[
                 InlineKeyboardButton("✛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ࿇", url=f"https://t.me/{bot.username}?startgroup=Bots4Sale&admin=invite_users+manage_chat")
                 ],[
-                InlineKeyboardButton("💥 Uᴘᴅᴀᴛᴇ 💥", url="https://t.me/+sT56Fh1VXZ81ZWRl")
-                ],[
+                InlineKeyboardButton("💥 Uᴘᴅᴀᴛᴇ 💥", url="https://t.me/+sT56Fh1VXZ81ZWRl"),    
                 InlineKeyboardButton("🔎 Sᴜᴘᴘᴏʀᴛ 🔍", url="https://t.me/+sT56Fh1VXZ81ZWRl")
             ]]))
     
