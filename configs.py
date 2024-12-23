6fbf022c46a497bf532b6e56d36e5e4b664a132a
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-23 13:21:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -38,7 +38,6 @@ class Config:
     API_HASH = getenv("API_HASH", "e12f1473abbeaaf8302bf2692efd98f6")
     BOT_TOKEN = getenv("BOT_TOKEN", "7129284446:AAE8vzEaENMxF8m5NQqvOm9GOtPVdZkk76g")
  
-    FORCE_SUB = int(getenv("FORCE_SUB", None))
     ADMIN = list(map(int, getenv("ADMIN", "7425490417").split()))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002276401285"))
     
